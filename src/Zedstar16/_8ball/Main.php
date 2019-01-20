8a29ff1b371daec830f4d5b8afda697c8d505581
Zed <35542555+Zedstar16@users.noreply.github.com>
2019-01-20 14:13:00 +0000

    Trigger 8ball with prefix
    Big changes:)
    Made the plugin fairly configurable, added cooldowns, a task, prefixes, edit bot message

--- src/Zedstar16/_8ball/Main.php
+++ src/Zedstar16/_8ball/Main.php
@@ -4,46 +4,76 @@ declare(strict_types=1);
 
 namespace Zedstar16\_8ball;
 
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as C;
+use Zedstar16\_8ball\tasks\PredictionTask;
+class Main extends PluginBase implements Listener {
 
-class Main extends PluginBase {
+    /**@var array*/
+    public $cooldown = [];
+    /**@var array*/
+    public $choices = array(
+        "It is certain" => "a",
+        "It is decidedly so" => "b",
+        "Without a doubt" => "c",
+        "Yes - definitely" => "d",
+        "You may rely on it" => "e",
+        "As I see it, yes" => "f",
+        "Most likely" => "g",
+        "Outlook good" => "h",
+        "Yes" => "i",
+        "Reply hazy, try again" => "j",
+        "Signs point to yes" => "k",
+        "Ask again later" => "l",
+        "Better not tell you now" => "m",
+        "Cannot predict now" => "n",
+        "Concentrate and ask again" => "o",
+        "Don't count on it" => "p",
+        "My reply is no" => "q",
+        "My sources say no" => "r",
+        "Outlook not so good" => "s",
+        "Very doubtful" => "t"
+    );
+
+    public function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveResource("config.yml");
+        $this->saveDefaultConfig();
+    }
+
+
+    public function onChat(PlayerChatEvent $event){
+        $pn = $event->getPlayer()->getName();
+
+        if(strpos($event->getMessage(), $this->getConfig()->get("bot-prefix")) !== false){
+            $ct = floatval($this->getConfig()->get("prediction-cooldown"));
+            if((!isset($this->cooldown[$pn])) || (($this->cooldown[$pn] + $ct - time() <= 0))){
+                $this->cooldown[$pn] = time();
+                //there is a delayed task because otherwise the prediction will send before the players message
+                $this->getScheduler()->scheduleDelayedTask(new PredictionTask($this), 2);
+            }else{
+                $event->setCancelled(true);
+                $event->getPlayer()->sendMessage(C::RED . "Don't try and predict anything too quickly! Wait ".C::WHITE . ($this->cooldown[$pn] + 10 - time()) . " seconds ".C::RED."before trying to make another prediction ;)");
+            }
+        }
+    }
 
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch($command->getName()){
 			case "8ball":
                 if(isset($args[0])) {
-                    $this->getServer()->getPlayer($sender->getName())->chat("8ball, ".implode(" ", $args));
-                    $choices = array(
-                        "It is certain" => "a",
-                        "It is decidedly so" => "b",
-                        "Without a doubt" => "c",
-                        "Yes - definitely" => "d",
-                        "You may rely on it" => "e",
-                        "As I see it, yes" => "f",
-                        "Most likely" => "g",
-                        "Outlook good" => "h",
-                        "Yes" => "i",
-                        "Reply hazy, try again" => "j",
-                        "Signs point to yes" => "k",
-                        "Ask again later" => "l",
-                        "Better not tell you now" => "m",
-                        "Cannot predict now" => "n",
-                        "Concentrate and ask again" => "o",
-                        "Don't count on it" => "p",
-                        "My reply is no" => "q",
-                        "My sources say no" => "r",
-                        "Outlook not so good" => "s",
-                        "Very doubtful" => "t"
-                    );
-                    $this->getServer()->broadcastMessage("§6➤ §8[§l§6Bot§r§8] §c8ball§8>§e " . array_rand($choices, 1) );
-
-                  
+                    $this->getServer()->getPlayer($sender->getName())->chat($this->getConfig()->get("bot-prefix") . " " . implode(" ", $args));
                 } else $sender->sendMessage("I need something to predict!");
             }
         return true;
     }
 
 }
+
